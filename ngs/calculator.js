b786e1cabcf2bf5c0299d5011265818ca7f48080
Gattxxa <mail@gattxxa.org>
2021-08-29 21:59:24 +0900

    ngs

---
+++ ngs/calculator.js
@@ -0,0 +1,15 @@
+$(function(){
+    $('#calc').on('click', function() {
+        let weapon =  parseFloat($('#weapon').val());
+        let unit1 =  parseFloat($('#unit1').val());
+        let unit2 =  parseFloat($('#unit2').val());
+        let unit3 =  parseFloat($('#unit3').val());
+
+        let step1 = weapon
+        let step2 = step1 + (unit1+(unit1*(step1/100)))
+        let step3 = step2 + (unit2+(unit2*(step2/100)))
+        let total = step3 + (unit3+(unit3*(step3/100)))
+
+        $("#result").text(total)
+    })
+});
