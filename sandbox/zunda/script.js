6170f8a948dc6f73fbf81eaa70c3597d88a9ea5d
Gattxxa.org <61118664+Gattxxa@users.noreply.github.com>
2023-05-03 09:13:02 +0900

    Add files via upload

--- sandbox/zunda/script.js
+++ sandbox/zunda/script.js
@@ -1,20 +1,19 @@
 $(function(){
 
+    // IDから計算
     $('#calcById').on('click', function() {
         result = 1
-        $('#calcByIdResult').text(
-            result *
-            $('#Data1').val() * 
-            $('#Data2').val() * 
-            $('#Data3').val()
-        );
+        result *= $('#Data1').val() * $('#Data2').val() * $('#Data3').val() // ...
+        $('#calcByIdResult').text(result)
     })
 
+    // Classから計算
     $('#calcByClass').on('click', function() {
         result = 1
-        Array.from($('.calc-target')).forEach(v => {
-            result *= v.value;
+        Array.from($('.calc-target')).forEach(e => {
+            result *= e.value;
         });
         $('#calcByClassResult').text(result);
     })
+
 });
